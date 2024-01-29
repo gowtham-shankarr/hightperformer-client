eed3bacf8bd693ea5566abace4f1350f69b7bb3b
Gowtham <gowthamshankar93@outlook.com>
2024-01-29 16:24:33 +0530

    changes

--- src/layout/SubHeader.jsx
+++ src/layout/SubHeader.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { MdKeyboardArrowDown, MdOutlineFileDownload } from 'react-icons/md';
 import { GoCommentDiscussion } from 'react-icons/go';
 import { IoSettingsOutline } from 'react-icons/io5';
-import DropdownMenu from '../components/dropdownMenu.jsx';
+import DropdownMenu from '../components/dropdownMenu';
 
 const SubHeader = () => {
   const [showDropdown, setShowDropdown] = useState({
